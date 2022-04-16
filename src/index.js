9a91f3e145b60222ac8d0582c772706963ecc834
JCMP92 <arq_juanmp@outlook.com>
2022-04-15 19:57:18 -0500

    Sow stored tasks working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import  {projects, buttonEvents}  from "./code/projects";
+import { tasks, taskButtonEvents, createTask } from "./code/tasks";
 
 
 import'normalize.css';
 import"./style/style.scss";
 
 console.log(projects.projectsContainer);
+
+
+createTask('JAVASCRIPT', 'Code');
 
